3c1ba4d57fabe2dce6a50be7dc9ceb0041d1a7e8
Tyler Martinez <tymartin@mines.edu>
2017-10-01 20:56:21 -0600

    Finished input. I don't think the data structures are the ones we wanted, but that's easily fixed

--- Algobowl/main.cpp
+++ Algobowl/main.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 #include <vector>
 #include <set>
 #include <map>
 using namespace std;
 
 int bestWeight;
+int n;
+int m;
+set<int> U;
+vector<int> weights;
+vector<set<int>> subsets;
 vector<int> solutionIDs;
 
 int main() {
@@ -15,7 +21,25 @@ int main() {
 
 void ReadInputs(string filename) {
 	ifstream inputs(filename);
+	string input;
+	inputs >> input;
+	n = stoi(input);		//Size of universal set
+	for (int i = 1; i <= n; i++) {		//Construct Universal set
+		U.insert(i);
+	}
+	inputs >> input;
+	m = stoi(input);		//Number of subsets
 
+	while (!inputs.eof()) {
+		set<int> temp;
+		while (inputs.peek() != '/n') {		//Build subset
+			inputs >> input;
+			temp.insert(stoi(input));
+		}
+		subsets.push_back(temp);	//Add subset to vector
+		inputs >> input;
+		weights.push_back(stoi(input));		//Add weight to vector at same index. This links their index as a pseudo-ID
+	}
 	inputs.close();
 }
 
