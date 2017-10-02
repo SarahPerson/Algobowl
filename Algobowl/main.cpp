e8ee909d4b69f4f0b35dce33a1de49c9d8765253
Tyler Martinez <tymartin@mines.edu>
2017-10-01 20:12:17 -0600

    Basic in/out, relearning git

--- Algobowl/main.cpp
+++ Algobowl/main.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <set>
+#include <map>
 using namespace std;
+
+int bestWeight;
+vector<int> solutionIDs;
 
 int main() {
     cout << "Hello World!\n";
     return 0;
+}
+
+void ReadInputs(string filename) {
+	ifstream inputs(filename);
+
+	inputs.close();
+}
+
+void WriteSolution() {
+	ofstream outputs("ourSolution.txt");
+	outputs << bestWeight << endl;
+	for (int i = 0; i < solutionIDs.size(); i++) {
+		outputs << solutionIDs.at(i) << " ";
+	}
+	outputs.close();
+}
+
+void SolutionChecker() {
+	ReadInputs("ourInput.txt");
+
 }
